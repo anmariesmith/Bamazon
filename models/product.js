58c37cce3f507f2bad3fb8932c82cea853977470
Anmarie Smith <anmariesmith007@gmail.com>
2018-11-06 15:34:46 -0500

    adds all the file structure,api routes, html routes, index.js code, server.js code

---
+++ models/product.js
@@ -0,0 +1,12 @@
+module.exports = function(connection, Sequelize) {
+    const Product = connection.define('Product', {
+      product_name: Sequelize.STRING,
+      department_name: Sequelize.STRING,
+      price: Sequelize.STRING,
+      stock_quantity: Sequelize.INTEGER
+    },{ timestamps: false
+    
+    });
+  
+    return Product;
+  }
